941f595309f5b9f24b7e3bbfea524d66f9530d8b
Anderson Danilo <contact@andersondanilo.com>
2020-10-13 22:14:02 -0300

    add salt (crypt service hash)

--- src/bundles/app_bundle/service/crypt_service.cr
+++ src/bundles/app_bundle/service/crypt_service.cr
@@ -2,13 +2,20 @@ require "crypto/bcrypt/password"
 
 module App
   class CryptService
+    def initialize(@app_secret : String)
+    end
+
+    def salt
+      "_salt_#{@app_secret}"
+    end
+
     def hash(password : String) : String
-      Crypto::Bcrypt::Password.create(password).to_s
+      Crypto::Bcrypt::Password.create(password + salt).to_s
     end
 
     def cmp_hash(crypted_hash : String, not_crypted_hash : String) : Bool
       password = Crypto::Bcrypt::Password.new(crypted_hash)
-      password.verify(not_crypted_hash)
+      password.verify(not_crypted_hash + salt)
     end
   end
 end
